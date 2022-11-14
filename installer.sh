d903fa22efe014896ffe9031abe1c1276b9eef34
0n1cOn3 <0n1cOn3@gmx.ch>
2022-11-14 02:48:44 +0100

    tiny code refactoring

--- installer.sh
+++ installer.sh
@@ -3,8 +3,9 @@
 # Modified by: 0n1coOn3
 # Version 0.2
 
-pkg install figlet -y 
-
+# Install dependency
+pkg install figlet -y 2&> /dev/null
+# Get Colors ready
 red='\e[1;31m'
 green='e[1;32m'
 blue='\e[1;34m'
@@ -14,44 +15,46 @@ echo -e $red "figlet Termux-Wifi"
 echo -e $blue "by MrBlackx/TheMasterCH"
 echo -e $purple "modified by 0n1cOn3"
 
+prep_env (){
 termux-setup-storage
 apt install git curl python2 python3
 clear
+}
 
+prep2_env (){
 apt install figlet wget proot tar curl -y
 # new Repo needs to be used cause the owner seems to discontinue the project (Readonly)wget https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh
 wget https://raw.githubusercontent.com/tuanpham-dev/termux-ubuntu/master/ubuntu.sh
 chmod +x ubuntu.sh
 bash ubuntu.sh
 clear
+}
 
+inst_env () {
 echo -e $purple ================================================================
 echo " "
 echo -e $blue && figlet "Installing requirements...
 echo " "
 echo -e $purple ================================================================
-
 chmod +rwx ubuntu.sh
 ./ubuntu.sh
 clear
 echo -e $purple "Successfully installed!"
 sleep 3
 ./start-ubuntu.sh
-clear
-
 apt-get update
 apt-get upgrade -y
 apt-get install git net-tools wireless_tools aircrack-ng xterm isc-dhcp-server reaver ettercap ettercap-text-only ettercap-graphical dsniff hostapd iptables bully ssltrip unzip expect expect-dev lighttpd hashcat pixiewps curl pip pip3 iwconfig php-cgi -y
 dhcpd -y
-
-# cloning fluxion
 git clone https://www.github.com/FluxionNetwork/fluxion.git
 cd fluxion && chmod +rwx *
 ./fluxion.sh
-
 echo -e $red "by MrBlackX/TheMasterCH"
 echo -e $purple "modified by 0n1cOn3
 echo -e $green "Successfully installed!"
+}
 
-
+prep_env
+prep2_env
+inst_env
 
